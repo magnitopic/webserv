1d3b88f0f033115a189fa9ab61a79a398cd1288d
javiburn <jsarabia98@gmail.com>
2024-01-18 13:09:35 +0100

    Socket class now has constructors

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 12:58:32 by jsarabia          #+#    #+#             */
-/*   Updated: 2024/01/18 13:01:03 by jsarabia         ###   ########.fr       */
+/*   Updated: 2024/01/18 13:08:28 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 # define SOCKET_HPP
 
 class Socket{
-	g
+	private:
+	public:
+		Socket(void);
+		Socket(const Socket& socket);
+		Socket& operator=(const Socket& socket);
+		~Socket(void);
 };
 
 #endif
