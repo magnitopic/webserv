b52493b4f7b5dc6129e8ca27f9b58139b50430d9
magnitopic <magnitopic@gmail.com>
2024-01-24 14:59:41 +0100

    location class

--- src/model/Server.hpp
+++ src/model/Server.hpp
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
@@ -6,22 +6,27 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 12:42:38 by alaparic          #+#    #+#             */
-/*   Updated: 2024/01/24 12:54:18 by alaparic         ###   ########.fr       */
+/*   Updated: 2024/01/24 14:46:39 by alaparic         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef SERVER_HPP_
 #define SERVER_HPP_
 
 #include "../../include/webserv.hpp"
+#include "Location.hpp"
 
 class Server
 {
 private:
-	unsigned int port;
-	std::string name;
-	std::map<int, std::string> errorPages;
-	unsigned long maxClientBodySize;
+	unsigned int port; // listen
+	std::string name; // server_name
+	std::string root; // root
+	std::string index; // index
+	std::string host; // host
+	std::map<int, std::string> errorPages; // error_page
+	unsigned long maxClientBodySize; // client_max_body_size
+	std::vector<Location> locations; // location
 	
 
 public:
