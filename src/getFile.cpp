120c68f394eded776f83efc37ea435dab83b2ede
magnitopic <magnitopic@gmail.com>
2024-02-26 19:26:40 +0100

    small fix

--- src/getFile.cpp
+++ src/getFile.cpp
@@ -1,21 +1,21 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   getFile.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:57:01 by alaparic          #+#    #+#             */
-/*   Updated: 2024/02/08 14:04:37 by jsarabia         ###   ########.fr       */
+/*   Updated: 2024/02/26 19:20:57 by alaparic         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../include/webserv.hpp"
 
 std::string getFile(const std::string &fileAdr)
 {
 	std::ifstream file(fileAdr, std::ios::in | std::ios::binary);
-	std::string content;
+	std::string content = "";
 
 	if (file.is_open()) {
 		std::stringstream buffer;
