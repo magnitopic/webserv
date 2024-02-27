e7cd0e9b4d6a1b306e8cefdb56e357f96620e859
magnitopic <magnitopic@gmail.com>
2024-02-27 18:45:35 +0100

    working on cgi tests

--- src/cgi.cpp
+++ src/cgi.cpp
@@ -6,20 +6,29 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 18:55:25 by alaparic          #+#    #+#             */
-/*   Updated: 2024/02/26 19:44:46 by alaparic         ###   ########.fr       */
+/*   Updated: 2024/02/27 18:44:25 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../include/webserv.hpp"
+#define MAX_CGI_TIME 5
 
-bool runPythonScript(char *route, char **args, char **env)
+void raiseError(const char *msg)
+{
+	std::cerr << RED << std::endl;
+	perror(msg);
+	std::cerr << RESET << std::endl;
+	exit(1);
+}
+
+bool cgiForPostReq(char *route, char **args, char **env)
 {
 	int fds[2];
 	pid_t id;
 
 	if (pipe(fds) == -1)
 		raiseError("CGI pipe error");
-	if (id = fork() == -1)
+	if ((id = fork()) == -1)
 		raiseError("CGI fork error");
 
 	// child process executes the python CGI
@@ -28,11 +37,49 @@ bool runPythonScript(char *route, char **args, char **env)
 		close(fds[0]);
 		dup2(fds[1], STDOUT_FILENO);
 		close(fds[1]);
-		execve(route, args ,env);
+		execve(route, args, env);
 		perror("execve");
-		return 1;
+		return false;
 	}
+	// original process
+	close(fds[1]);
 
-	close (fds[1]);
-	
+	bool cgiFinished = 0;
+	pid_t result;
+	time_t cgiTime = time(NULL);
+	if (cgiTime == -1)
+		raiseError("error getting time");
+
+	while (!cgiFinished)
+	{
+		result = waitpid(id, NULL, WNOHANG);
+
+		if (result == 0)
+		{
+			time_t auxTime = time(NULL);
+			if (auxTime == -1)
+				raiseError("error getting time");
+			if ((auxTime - cgiTime) > MAX_CGI_TIME)
+			{
+				kill(id, SIGKILL);
+				close(fds[0]);
+				std::cerr << BLUE << "==> ❌ " << RED << "CGI ERROR: Infinite loop" << std::endl;
+				return false;
+			}
+		}
+		else if (result == -1)
+		{
+			std::cerr << BLUE << "==> ❌ " << RED << "CGI ERROR: Error executing script" << std::endl;
+		}
+		else
+			cgiFinished = !cgiFinished;
+	}
+	return true;
 }
+
+int main(int argc, char **argv, char **env)
+{
+	cgiForPostReq(*argv, argv, env);
+	return 0;
+}
+
