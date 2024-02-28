62bed6dc293ec4dab210a8f41ad3b035f7f432b3
magnitopic <magnitopic@gmail.com>
2024-02-28 17:48:55 +0100

    greeting user cgi

--- pages/cgi-bin/greet_user.py
+++ pages/cgi-bin/greet_user.py
@@ -1,4 +1,16 @@
-print(5 + 5)
-print("\nGOD")
-print("<br>FRESH 🥶")
-print("<ol><li>Furbo</li><li>Fiesta</li><li>🍹</li></ol>\n")
+import os
+path_info = os.environ['PATH_INFO']
+
+path_info = path_info[1:]
+path_info = path_info.split("&")
+for i in range(len(path_info)):
+	path_info[i] = path_info[i].split("=")
+
+
+print(f"<h1>Hola {path_info[0][1]}, bienvenido</h1>")
+
+for i in path_info:
+	print("<p>")
+	for j in i:
+		print(j)
+	print("</p>")
